1631febe1cda32b61544e7e0670940c9207678ee
JepCast <ycastillop2@miumg.edu.gt>
2023-08-03 12:09:25 -0600

    Agregue diversas escenas con diversos botones para el cambio y movimiento entre estas

--- src/main/java/parcial/parcial1_java/HelloApplication.java
+++ src/main/java/parcial/parcial1_java/HelloApplication.java
@@ -2,28 +2,54 @@ package parcial.parcial1_java;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+
 import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root= FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        Scene scene = new Scene(root);
+
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene1.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
         Image icon = new Image("C:\\Universidad 1\\Java\\Parcial1_java\\src\\iconapp.jpeg"); // This is the icon of the window
         stage.getIcons().add(icon); //And this adds the icon to the window
-        stage.setTitle("Minecraft Dream"); //This is the title of the window
+        stage.setWidth(500); //This is the width of the window
+        stage.setHeight(535); //This is the height of the window
+        final String[] username = new String[1];
 
 
+            // Crear el TextField
+            TextField inNameTexField = new TextField();
 
+            // Asignar un evento para guardar el texto al precionar Enter
+            inNameTexField.setOnAction(e -> {
+
+                username[0] = inNameTexField.getText();
+            });
+
+            Button AcceptButton = new Button("Aceptar");
+            AcceptButton.setOnAction(e -> {
+                username[0] = inNameTexField.getText(); // Guardar el nombre de usuario ingresado
+
+            });
         stage.setScene(scene);
         stage.show();
-    }
 
+
+    }
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/parcial/parcial1_java/HelloController.java
+++ src/main/java/parcial/parcial1_java/HelloController.java
@@ -1,14 +1,118 @@
 package parcial.parcial1_java;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class HelloController {
+
+    public Button AcceptButton1;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene3(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene3.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene4(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene4.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene5(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene5.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene6(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene6.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene7(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene7.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene8(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene8.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene9(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene9.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene10(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene10.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+    public AnchorPane scene1;
     @FXML
-    private Label welcomeText;
+    public TextField inNameTexField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label AcceptButton;
+
+    @FXML
+    public void AcceptButton1(ActionEvent OnAction) {
+        String nameuser = inNameTexField.getText();
+        AcceptButton.setText("Tu nombre ha sido guardado!");
+    }
+
+    public void inNameText(ActionEvent OnEnter) {
+        String nameuser = inNameTexField.getText();
+        inNameTexField.setText("Bienvenido " + nameuser + "!");
     }
 }
